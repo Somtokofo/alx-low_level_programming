26b6e2ba1dbd50c8ced6dc240945b5e8085fc25d
Somtokofo <56971259+Somtokofo@users.noreply.github.com>
2022-03-16 05:34:31 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,14 +14,14 @@ void print_alphabet_x10(void)
 	while (alp_count < 10)
 	{
 		char ch = 'a';
-		
+
 		do {
 			_putchar(ch);
 			ch++;
 		} while (ch <= 'z');
-		
+
 		alp_count++;
-		
+
 		_putchar('\n');
 	}
 }
