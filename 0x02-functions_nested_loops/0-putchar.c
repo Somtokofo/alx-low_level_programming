c2a20d88c5978a3f8cc84b0f141c329e80010eb0
Somtokofo <56971259+Somtokofo@users.noreply.github.com>
2023-09-13 00:57:28 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-  char chr[8] = "_putchar";
-  int i = 0;
+	char chr[8] = "_putchar";
+	int i = 0;
 
-  while (i < 8)
-  {
-    _putchar(chr[i]);
-    i++;
-  }
-  _putchar('\n');
-  return (0);
+	while (i < 8)
+	{
+		_putchar(chr[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
