ad8c5e1d88fd3da40774247bb0bea5592d162770
Somtokofo <skofojr@gmail.com>
2023-09-13 19:47:56 +0100

    print 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+/**
+ * print_to_98 - Check Holberton
+ * @n: A input integer
+ * Description: function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+    if (n >= 98)
+    {
+		while (n >= 98)
+		{
+			printf("%d", n);
+			printf(", ");
+			n--;
+		}
+		printf("\n");
+    }
+    else
+    {
+		while (n < 98)
+		{
+			printf("%d", n);
+			printf(", ");
+			n++;
+		}
+		printf("\n");
+    }
+}
