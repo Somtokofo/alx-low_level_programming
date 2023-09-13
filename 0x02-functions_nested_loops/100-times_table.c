3b8249737d0d6e958b9bcbb343d26b478e314389
Somtokofo <skofojr@gmail.com>
2023-09-13 23:12:18 +0100

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,21 +28,23 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(k + '0');
 			}
-			else if (k >= 10 && k < 100)
+			else 
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			else if (k >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(((k % 100) / 10) + '0');
-				_putchar(((k % 100) % 10) + '0');
+
+				if (k >= 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k % 100) / 10) + '0');
+					_putchar(((k % 100) % 10) + '0');
+				}
 			}
 		}
 		_putchar('\n');
