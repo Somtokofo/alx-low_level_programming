81ff6b71efbe32c57edef43d7db86b1d7afb6dd3
Somtokofo <skofojr@gmail.com>
2023-09-13 22:38:57 +0100

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,12 +2,12 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * If n is greater than 15 or less than 0, the function should not print anything
+ * @n: input number as integer
  */
 void print_times_table(int n)
 {
 	int i, j, k;
-	
+
 	if (n < 0 || n > 15)
 		return;
 
