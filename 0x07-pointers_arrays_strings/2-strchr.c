f7ac9698f47e74934efe59852760ff51c1f9539b
Somtokofo <56971259+Somtokofo@users.noreply.github.com>
2023-09-28 12:32:02 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
-	i = 0;
+	unsigned int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
 			return (s + i);
-		}
-		i++;
-	}
-	return('\0');
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
