0c924b63d87009926761e9ab79cd3025a710b0fe
Somtokofo <56971259+Somtokofo@users.noreply.github.com>
2023-09-28 12:21:26 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,13 +6,13 @@
  * @n: bytes filled.
  * Return: the pointer to dest.
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 	i = 0;
 
-	while (i < n)
-	{
+	while (i < n){
 		s[i] = b;
 		i++;
 	}
